ff1af8f8033bf7a4a8c7a0397b636632ad926dac
susanlin0705 <susan.lin0705@gmail.com>
2019-08-20 16:01:53 -0500

    added if/else with servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,18 +4,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("it's working");
-        res.setStatus(333);
-        try {
-            res.getWriter().println("\n"+"Hello World!"+"\n" );
-        }catch (IOException e){
-            e.printStackTrace();
+
+        String name = req.getParameter("name");
+//        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+//        String message= "<h1> Hello World!</h1>";
+//        out.println(message);
+
+        if(name!=null){
+            String word = "<h1>Hello "+ name +" !</h1>";
+            out.println(word);
+        }else{
+            String message= "<h1> Hello World!</h1>";
+            out.println(message);
         }
+
+
+
+
+//        res.setStatus(333);
+//        try {
+//            res.getWriter().println("\n"+"<h1>Hello World!</h1>"+"\n" );
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
 
     }
 
