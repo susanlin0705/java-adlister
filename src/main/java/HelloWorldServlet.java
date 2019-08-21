360f6f98ddb4efb06c8664548a27c088a2da200d
susanlin0705 <susan.lin0705@gmail.com>
2019-08-21 10:50:45 -0500

    added webapp and also redo the servlet exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,38 +8,49 @@ import java.io.PrintWriter;
 
 
 @WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        System.out.println("it's working");
-
-        String name = req.getParameter("name");
-//        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-//        String message= "<h1> Hello World!</h1>";
-//        out.println(message);
-
-        if(name!=null){
-            String word = "<h1>Hello "+ name +" !</h1>";
-            out.println(word);
-        }else{
-            String message= "<h1> Hello World!</h1>";
-            out.println(message);
-        }
-
-
-
-
-//        res.setStatus(333);
-//        try {
-//            res.getWriter().println("\n"+"<h1>Hello World!</h1>"+"\n" );
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
+public class HelloWorldServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String name= req.getParameter("name");
+       resp.setContentType("text/html");
+       PrintWriter out = resp.getWriter();
+       if(name != null){
+           String world ="<h1>Hello "+ name + "!</h1>";
+           out.println(world);
+       }else{
+           String message ="<h1>Hello World !</h1>";
+           out.println(message);
+       }
 
     }
 
+
+
+//@WebServlet("/hello")
+//public class HelloWorldServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        System.out.println("it's working");
+//
+//        String name = req.getParameter("name");
+////        res.setContentType("text/html");
+//        PrintWriter out = res.getWriter();
+////        String message= "<h1> Hello World!</h1>";
+////        out.println(message);
+//
+//        if(name!=null){
+//            String word = "<h1>Hello "+ name +" !</h1>";
+//            out.println(word);
+//        }else{
+//            String message= "<h1> Hello World!</h1>";
+//            out.println(message);
+//        }
+//
+//
+//    }
+//
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 //        super.doPost(req, resp);
         System.out.println("does it work?");
         try {
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,36 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: susan
+  Date: 2019-08-21
+  Time: 09:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%--<%@ page contentType="text/html;charset=UTF-8" language="java" %>--%>
+<%--<html>--%>
+<%--<head>--%>
+<%--    <title>Title</title>--%>
+<%--</head>--%>
+<%--<body>--%>
+
+<%--</body>--%>
+<%--</html>--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%!int counter = 0; %>
+<% counter += 1; %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h1>The current count is <%= counter %>.</h1>
+
+View the page source!
+
+<%-- this is a JSP comment, you will *not* see this in the html --%>
+
+<!-- this is an HTML comment, you *will* see this in the html -->
+
+
+</body>
+</html>
